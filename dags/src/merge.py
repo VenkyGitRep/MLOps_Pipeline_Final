420766bd8f6abf860e0fc60e4c096a3916416ed2
Venky Sundar <venks.mail@gmail.com>
2024-04-19 18:27:35 -0400

    Added logging for merge. Making sure merge happins only when there is more than one file to merge.

--- dags/src/merge.py
+++ dags/src/merge.py
@@ -7,14 +7,19 @@ f_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..'))
 
 fin_path = os.path.join(f_dir, 'output')
 fout_path = os.path.join(f_dir, 'data')
+
 def merg_files(input = fin_path,output = fout_path):
     file_list = glob.glob(f'{input}/*.xlsx')
+    print("file_list:",file_list)
     excel_list = []
     for file in file_list:
         excel_list.append(pd.read_excel(file))
-    excel_merged = pd.concat(excel_list,ignore_index=True)
-    excel_merged.to_csv(f'{output}/Online_Retail.csv')
+    print(len(excel_list))
+    if(len(excel_list)>0):
+        excel_merged = pd.concat(excel_list,ignore_index=True)
+        excel_merged.to_csv(f'{output}/Online_Retail.csv')
+
     
-# if __name__ == "__main__":
-#     unzip_f = merg_files(fin_path,fout_path)
+if __name__ == "__main__":
+    unzip_f = merg_files(fin_path,fout_path)
     
